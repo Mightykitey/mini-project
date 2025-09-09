a54b1775cf97df0ff36264b8c6765e740f5a497f
Milvus Milvus <138457184+Mightykitey@users.noreply.github.com>
2025-09-09 10:27:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo"<!doctype html>";
+echo"<html lang=\"en\">";
+echo"<head><title>WIP</title>";
+echo"<link rel='stylesheet' href='css/styles.css'>";
+
+echo"</head>";
+echo"<body>";
+    echo"<h1>Black holes</h1>";
+    echo"<p>test</p>";
+
+echo"</body>";
+echo"</html>";
+
